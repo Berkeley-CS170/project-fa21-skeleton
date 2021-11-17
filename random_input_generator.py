44d6e92c5a7679e50b05b0752612433d25018c3d
William Furtado <willfurtado@berkeley.edu>
2021-11-16 23:24:17 -0800

    Inputs

--- random_input_generator.py
+++ random_input_generator.py
@@ -1,20 +1,33 @@
-import Task
+import numpy as np
 import parse
-import random
+import Task
 
-def random_generate_input_file(num_inputs):
+
+def random_generate_input_file(num_inputs, fname):
     """
-    Randomly generates an input file according to num_inputs
+    Randomly generates an input file according to num_inputs and writes to a
+    file with name fname
     """
-    tasks = []
-    for i in range(1, num_inputs + 1):
-        deadline = random.randint(1, 1440)
-        duration = random.randint(1, 60)
-        perfect_benefit = round(random.random() * 100, 3)
-        task = Task.Task(task_id=i, deadline=deadline, duration=duration, perfect_benefit=perfect_benefit)
-        tasks.append(task)
-    parse.write_input_file("randomized_input_example", tasks)
+    indices = np.arange(1, num_inputs + 1)
+    deadlines = np.random.randint(1, 1440, size=num_inputs)
+    durations = np.random.randint(1, 60, size=num_inputs)
+    perfect_benefits = np.round(np.random.uniform(0, 100, size=num_inputs), 3)
 
-n = 100
+    tasks = [
+        Task.Task(
+            task_id=i,
+            deadline=deadline,
+            duration=duration,
+            perfect_benefit=perfect_benefit,
+        )
+        for i, deadline, duration, perfect_benefit in zip(
+            indices, deadlines, durations, perfect_benefits
+        )
+    ]
 
-random_generate_input_file(n)
+    parse.write_input_file(fname, tasks)
+
+
+if __name__ == "__main__":
+    for input_size in (100, 150, 200):
+        random_generate_input_file(input_size, f"{input_size}.in")
